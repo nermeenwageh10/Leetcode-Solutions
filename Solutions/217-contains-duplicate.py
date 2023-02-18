6418c55aaf050dc5249e3b627c40d35237aa8aac
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-02-18 20:17:42 +0100

    Update 217-contains-duplicate.py

--- Solutions/217-contains-duplicate.py
+++ Solutions/217-contains-duplicate.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashSet=set()
+        
+        for n in nums :
+            if n in hashSet:
+                return True
+            hashSet.add(n)
+        return False
+      
+    #Time complexity : O(N)
+    #Space complexity : O(N)
