e66ba8c9e5746aa96f21408ddf502b12a2d1153a
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-02-20 22:07:31 +0100

    Update 104-maximum-depth-tree.py

--- Solutions/104-maximum-depth-tree.py
+++ Solutions/104-maximum-depth-tree.py
@@ -6,3 +6,5 @@ class Solution:
             return max(rec(node.left,s+1),rec(node.right,s+1))
         maxs=rec(root,0)
         return maxs
+      #Time Complexity :O(N)
+      #Space Complexity:O(1)
