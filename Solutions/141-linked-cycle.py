9e6c56fb09470cb2c1161f6c62a5b98ca2fafc50
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-02-20 21:56:55 +0100

    Create 141-linked-cycle.py

---
+++ Solutions/141-linked-cycle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+      #Time Complexity :O(N)
+      #Space Complexity:O(1)
