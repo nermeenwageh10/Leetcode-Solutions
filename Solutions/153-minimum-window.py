8a82ab0c1d933f1d0364c0d35c3e3748bac3fa7f
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-02-20 21:44:18 +0100

    Create 153-minimum-window.py

---
+++ Solutions/153-minimum-window.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
+      
+      #Time complexity:O(N)
+
+
+      #Space complexity:O(1)
