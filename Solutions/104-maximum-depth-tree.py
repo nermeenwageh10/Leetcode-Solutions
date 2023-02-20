f31b1c46eb574379c04bb23dd9d84dcbace617f6
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-02-20 22:06:19 +0100

    Create 104-maximum-depth-tree.py

---
+++ Solutions/104-maximum-depth-tree.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def rec(node,s):
+            if node is None:
+                return s
+            return max(rec(node.left,s+1),rec(node.right,s+1))
+        maxs=rec(root,0)
+        return maxs
