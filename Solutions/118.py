aaa8a33c497d5ce7afbab511f81d7fcaa2ddc10c
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-02-25 23:15:54 +0100

    Update 118.py

--- Solutions/118.py
+++ Solutions/118.py
@@ -1,13 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res=[[1]]
-        level=[]
-        for i in range(1,numRows):
-            level=[1]*(i+1)
-            for l in range(len(level)):
-                if l == 0 or l==len(level)-1:
-                    continue
-                else:
-                    level[l]=res[-1][l]+res[-1][l-1]
-            res.append(level)
-        return res
+        Pascal = [[0 for j in range(i)] for i in range(1, numRows+1)]
+        for i in range(numRows):
+            row = [1] * (i+1)
+            for j in range(1, i):
+                row[j] = Pascal[i-1][j-1] + Pascal[i-1][j]
+            Pascal[i] = row
+        return Pascal
