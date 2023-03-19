abc0ed97adfd24baa3c0b6857516771d97915c0d
Nour eldien <49589347+Noureldien334@users.noreply.github.com>
2023-03-19 22:12:48 +0200

    Delete Reverse Linked List.cpp

--- Solutions/C++/Reverse Linked List.cpp
+++
@@ -1,9 +0,0 @@
- ListNode* curr = head;
-        ListNode* temp,*prev=NULL;
-        while(curr){
-            temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        return prev;
