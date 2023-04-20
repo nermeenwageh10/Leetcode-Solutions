5ead94a18e3a4cad5899c2a534360cac16931875
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-20 07:24:51 +0200

    Delete 108.py

--- Solutions/108.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        if not nums:
-            return None
-        mid = len(nums) // 2
-        root = TreeNode(nums[mid])
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
-        return root
