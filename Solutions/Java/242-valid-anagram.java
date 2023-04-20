2a0d26ab18806b5d41ce459e788485a124ceb836
Wissam Fawaz <wissamfawaz12@gmail.com>
2023-04-20 13:49:24 +0300

    Added Java solution to LeetCode 242 problem

---
+++ Solutions/Java/242-valid-anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> charsCount = new HashMap<>();
+
+        for (char letter : s.toCharArray()) {
+            charsCount.put(letter, charsCount.getOrDefault(letter, 0) + 1);
+        }
+
+        for (char letter : t.toCharArray()) {
+            if (!charsCount.containsKey(letter)) {
+                return false;
+            } else {
+                int count = charsCount.get(letter);
+                if (count == 0) {
+                    return false;
+                } else {
+                    charsCount.put(letter, count - 1);
+                }
+            }
+        }
+
+        for (Character letter : charsCount.keySet()) {
+            if (charsCount.get(letter) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
