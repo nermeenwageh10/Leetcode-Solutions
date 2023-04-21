455a0ee326ece0c04791ca478e015ad1e929b37c
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 14:04:40 +0200

    Delete 647-palindromic-substrings.py

--- Solutions/647-palindromic-substrings.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countSubstrings(self, s: str) -> int:
-	    L, r = len(s), 0
-	    for i in range(L):
-	    	for a,b in [(i,i),(i,i+1)]:
-	    		while a >= 0 and b < L and s[a] == s[b]: a -= 1; b += 1
-	    		r += (b-a)//2
-	    return r
