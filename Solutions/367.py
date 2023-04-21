9a4d5521af73c2a2b6d8a8ab811b20bd824685d2
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:56:28 +0200

    Delete 367.py

--- Solutions/367.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
-        num=num**0.5
-        while(num>=1):
-            if(num%1==0):
-                return(True)
-            else:
-                return(False)
-                
