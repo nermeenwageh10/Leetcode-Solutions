5b49d62469b4c7993e66ed008727398c4cd4f5c9
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:48:00 +0200

    Delete 977.py

--- Solutions/977.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums[i]=pow(nums[i],2)
-        return(sorted(nums))
-            
