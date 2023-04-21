db82c2e4c6730dff49a9bb59b9ff0551733e5437
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:47:21 +0200

    Delete 219-Contains-Duplicate.py

--- Solutions/219-Contains-Duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        d={}
-        for i in range(len(nums)):
-            if nums[i] in d:
-                if i -d[nums[i]] <= k:
-                    return(True)
-            d[nums[i]]=i
-        return(False)
