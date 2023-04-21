2e056d78ecbe59da4dc828dc5caf8feb83c23a11
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:53:05 +0200

    Delete 929-Unique-Email.py

--- Solutions/929-Unique-Email.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numUniqueEmails(self, emails: List[str]) -> int:
-        seen = set()
-        for email in emails:
-            local,domain=email.split('@')
-            local=local.split('+')[0]
-            local=local.replace('.',"")
-            seen.add(local+"@"+domain)
-        return(len(seen))
