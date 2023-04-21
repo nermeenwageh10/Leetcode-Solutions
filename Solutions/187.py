06590c848a0bdace75817cd82a971ed262cde44d
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:42:41 +0200

    Delete 187.py

--- Solutions/187.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        freq = {}
-        for i in range(len(s) - 9):
-            substring = s[i:i+10]
-            freq[substring] = freq.get(substring, 0) + 1
-        return [substring for substring in freq if freq[substring] > 1]
