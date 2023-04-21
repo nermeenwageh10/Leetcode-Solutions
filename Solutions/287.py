7c9f45b5537d56267bc611d1b6aca07befa3cdd3
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:57:32 +0200

    Delete 287.py

--- Solutions/287.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i]==nums[i+1]:
-                return (nums[i])
-        return(nums[0])
