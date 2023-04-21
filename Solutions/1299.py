0c3e25fbe9b47eba3c6dfe2c7ac0b2bcdd843451
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 12:34:35 +0200

    Delete 1299.py

--- Solutions/1299.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        rightMax=-1
-        for i in range(len(arr)-1,-1,-1):
-            intialValue=arr[i]
-            arr[i]=rightMax
-            rightMax=max(intialValue,arr[i])
-        return(arr)
