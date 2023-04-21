3796660d3bf49b2232203843682a8d872675690d
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:43:11 +0200

    Delete 189.py

--- Solutions/189.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        for i in range(k):
-            temp=nums[-1]
-            nums.pop(len(nums)-1)
-            nums.insert(0,temp)
-        return(nums)
