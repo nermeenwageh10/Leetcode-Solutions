c76c77de371169cf3ac3583c88cdedfe0964fa6b
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:53:38 +0200

    Delete 338-Counting-Bits.py

--- Solutions/338-Counting-Bits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        arr=[0]
-        for i in range(1,n+1):
-            value=bin(i).count('1')
-            arr.append(value)
-        return arr
