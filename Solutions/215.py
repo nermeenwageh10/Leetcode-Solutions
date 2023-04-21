cf68d080b81548323f0accd2a9fb789107f74b76
nermeenwageh10 <112512087+nermeenwageh10@users.noreply.github.com>
2023-04-21 13:47:02 +0200

    Delete 215.py

--- Solutions/215.py
+++
@@ -1,6 +0,0 @@
-import heapq
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heapq.heapify(nums)
-        res=heapq.nlargest(k,nums)
-        return(res[-1])
