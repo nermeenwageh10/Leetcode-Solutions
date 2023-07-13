be7cf9de74551c6a12da945d389cc735c054f84c
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-07-13 14:21:44 +0300

    fix: Problem no. 238 javascript (fix Syntax error)

--- Solutions/JavaScript/238. Product of Array Except Self.js
+++ Solutions/JavaScript/238. Product of Array Except Self.js
@@ -4,24 +4,25 @@
 We want to find arr the product of all the elements of nums except nums[i].  
 Solved by using prefix  Array .   complexity O(N)
 
-*/
+
   @param {number[]} nums
   @return {number[]}
- 
-var productExceptSelf = function(nums) {
-  if (!nums.length) return nums;
+ */
+  var productExceptSelf = function(nums) {
+    if (!nums.length) return nums;
 
-  const res = [1];
-  for (let i = 0; i  nums.length; i++) {
-    const x = res[res.length - 1]  nums[i];
-    res.push(x);
-  }
+    const res = [1];
+    for (let i = 0; i < nums.length; i++) {
+        const x = res[res.length - 1] * nums[i];
+        res.push(x);
+    }
 
-  let x = 1;
-  for (let i = nums.length-1; i =0; i--) {
-    res[i] = x  res[i];
-    x = nums[i]  x;
-  }
- res.pop(x);
- return res;
+    let x = 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] = x * res[i];
+        x = nums[i] * x;
+    }
+    res.pop(x);
+    return res;
 };
